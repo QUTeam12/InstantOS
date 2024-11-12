eb1175aebe59e9e1cf234bf8f31eee3ea077c921
Kaito Shigyo <shigyo.kaito.437@s.kyushu-u.ac.jp>
2024-11-12 16:09:43 +0900

    [finish]level2

--- typing.s
+++ typing.s
@@ -198,12 +198,6 @@ MAIN:
 ** システムコールによる RESET_TIMER の起動
 	move.l	#SYSCALL_NUM_RESET_TIMER,%D0
 	trap	#0
-
-	move.l	#SYSCALL_NUM_PUTSTRING, %d0
-	move.l	#0, %d1
-	move.l	#words, %d2
-	move.l	#3, %d3
-	trap	#0
 	
 ** タイピングする文字列の出力
 	move.l	#SYSCALL_NUM_PUTSTRING, %d0
